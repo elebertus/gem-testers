82c5fc75763be455934b754f83113d374025556a
Josiah Kiehl <bluepojo@gmail.com>
2011-02-27 15:35:44 -0500

    Removed logging for rack redirector

--- lib/domain_redirector.rb
+++ lib/domain_redirector.rb
@@ -4,9 +4,6 @@ class DomainRedirector
   end
 
   def call env
-    File.open('/tmp/request.log', 'a') do |file|
-      file.puts env.pretty_inspect
-    end
     parsed = URI::parse "http://#{env['HTTP_HOST']}"
     if parsed.host != 'test.rubygems.org'
       [301, {"Location" => "http://test.rubygems.org:#{env['SERVER_PORT']}#{env['PATH_INFO']}?#{env['QUERY_STRING']}"}, "Redirecting to test.rubygems.org..."]
