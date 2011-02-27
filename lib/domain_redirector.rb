ff75d81af4ff1c8d8b867d777502c6690fa2bf56
Josiah Kiehl <bluepojo@gmail.com>
2011-02-27 15:17:31 -0500

    log requests for the redirector

--- lib/domain_redirector.rb
+++ lib/domain_redirector.rb
@@ -4,6 +4,9 @@ class DomainRedirector
   end
 
   def call env
+    File.open('/tmp/request.log', 'a') do |file|
+      file.puts env.pretty_inspect
+    end
     if env['SERVER_NAME'] != 'test.rubygems.org'
       [301, {"Location" => "http://test.rubygems.org:#{env['SERVER_PORT']}#{env['PATH_INFO']}"}, "Redirecting to test.rubygems.org..."]
     else
