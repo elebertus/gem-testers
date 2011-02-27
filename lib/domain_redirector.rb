7994180fdc114572e5ac7c44340a4cbb04974bdd
Josiah Kiehl <bluepojo@gmail.com>
2011-02-27 15:33:34 -0500

    Query string and proper host parsing

--- lib/domain_redirector.rb
+++ lib/domain_redirector.rb
@@ -7,8 +7,9 @@ class DomainRedirector
     File.open('/tmp/request.log', 'a') do |file|
       file.puts env.pretty_inspect
     end
-    if env['SERVER_NAME'] != 'test.rubygems.org'
-      [301, {"Location" => "http://test.rubygems.org:#{env['SERVER_PORT']}#{env['PATH_INFO']}"}, "Redirecting to test.rubygems.org..."]
+    parsed = URI::parse "http://#{env['HTTP_HOST']}"
+    if parsed.host != 'test.rubygems.org'
+      [301, {"Location" => "http://test.rubygems.org:#{env['SERVER_PORT']}#{env['PATH_INFO']}?#{env['QUERY_STRING']}"}, "Redirecting to test.rubygems.org..."]
     else
       @app.call env
     end
