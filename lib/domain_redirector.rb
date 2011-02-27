f1fa3f979d43c8b54d1e2196a4e34e5ef1e2d5f0
Josiah Kiehl <bluepojo@gmail.com>
2011-02-27 14:47:34 -0500

    match with a not to cover all cases

--- lib/domain_redirector.rb
+++ lib/domain_redirector.rb
@@ -4,8 +4,7 @@ class DomainRedirector
   end
 
   def call env
-    name = env['SERVER_NAME']
-    if name == 'gem-testers.org' or name == 'testers.rubygems.org'
+    if env['SERVER_NAME'] != 'test.rubygems.org'
       [301, {"Location" => "http://test.rubygems.org:#{env['SERVER_PORT']}#{env['PATH_INFO']}"}, "Redirecting to test.rubygems.org..."]
     else
       @app.call env
