d001ce0331d302c7664843e9a9aec2f76235efa0
Erik Hollensbe <erik@hollensbe.org>
2011-03-06 01:01:00 -0500

    Reenable with fix for EY

--- lib/domain_redirector.rb
+++ lib/domain_redirector.rb
@@ -6,7 +6,14 @@ class DomainRedirector
   def call env
     parsed = URI::parse "http://#{env['HTTP_HOST']}"
     if parsed.host != 'test.rubygems.org'
-      [301, {"Location" => "http://test.rubygems.org:#{env['SERVER_PORT']}#{env['PATH_INFO']}?#{env['QUERY_STRING']}"}, "Redirecting to test.rubygems.org..."]
+
+      server_port = if ENV['SERVER_PORT'] == "81"
+                      "80"
+                    else
+                      ENV['SERVER_PORT']
+                    end
+
+      [301, {"Location" => "http://test.rubygems.org:#{server_port}#{env['PATH_INFO']}?#{env['QUERY_STRING']}"}, "Redirecting to test.rubygems.org..."]
     else
       @app.call env
     end
