d5afefc4438f77c3559b8c405efcb3b25b7a1ccd
Josiah Kiehl <bluepojo@gmail.com>
2011-04-22 21:50:19 -0400

    dev structure

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,202 +1,45 @@
---
--- PostgreSQL database dump
---
+CREATE TABLE `rubygems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_rubygems_on_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-SET search_path = public, pg_catalog;
+CREATE TABLE `test_results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `result` tinyint(1) NOT NULL,
+  `test_output` text COLLATE utf8_unicode_ci,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `version_id` int(11) NOT NULL,
+  `rubygem_id` int(11) NOT NULL,
+  `operating_system` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `architecture` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `machine_architecture` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `vendor` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ruby_version` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `platform` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `rubygems_test_version` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: rubygems; Type: TABLE; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE TABLE rubygems (
-    id integer NOT NULL,
-    name character varying(255) NOT NULL,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
-);
-
-
---
--- Name: rubygems_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE rubygems_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
---
--- Name: rubygems_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE rubygems_id_seq OWNED BY rubygems.id;
-
-
---
--- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE TABLE schema_migrations (
-    version character varying(255) NOT NULL
-);
-
-
---
--- Name: test_results; Type: TABLE; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE TABLE test_results (
-    id integer NOT NULL,
-    result boolean NOT NULL,
-    test_output text,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone,
-    version_id integer NOT NULL,
-    rubygem_id integer NOT NULL,
-    operating_system character varying(255),
-    architecture character varying(255),
-    machine_architecture character varying(255),
-    vendor character varying(255),
-    ruby_version character varying(255),
-    platform character varying(255)
-);
-
-
---
--- Name: test_results_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE test_results_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
---
--- Name: test_results_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE test_results_id_seq OWNED BY test_results.id;
-
-
---
--- Name: versions; Type: TABLE; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE TABLE versions (
-    id integer NOT NULL,
-    number character varying(255) NOT NULL,
-    rubygem_id integer NOT NULL,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone,
-    prerelease boolean
-);
-
-
---
--- Name: versions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE versions_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MAXVALUE
-    NO MINVALUE
-    CACHE 1;
-
-
---
--- Name: versions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE versions_id_seq OWNED BY versions.id;
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE rubygems ALTER COLUMN id SET DEFAULT nextval('rubygems_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE test_results ALTER COLUMN id SET DEFAULT nextval('test_results_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE versions ALTER COLUMN id SET DEFAULT nextval('versions_id_seq'::regclass);
-
-
---
--- Name: rubygems_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
---
-
-ALTER TABLE ONLY rubygems
-    ADD CONSTRAINT rubygems_pkey PRIMARY KEY (id);
-
-
---
--- Name: test_results_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
---
-
-ALTER TABLE ONLY test_results
-    ADD CONSTRAINT test_results_pkey PRIMARY KEY (id);
-
-
---
--- Name: versions_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
---
-
-ALTER TABLE ONLY versions
-    ADD CONSTRAINT versions_pkey PRIMARY KEY (id);
-
-
---
--- Name: index_rubygems_on_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE UNIQUE INDEX index_rubygems_on_name ON rubygems USING btree (name);
-
-
---
--- Name: index_versions_on_rubygem_id_and_number; Type: INDEX; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE UNIQUE INDEX index_versions_on_rubygem_id_and_number ON versions USING btree (rubygem_id, number);
-
-
---
--- Name: unique_schema_migrations; Type: INDEX; Schema: public; Owner: -; Tablespace: 
---
-
-CREATE UNIQUE INDEX unique_schema_migrations ON schema_migrations USING btree (version);
-
-
---
--- PostgreSQL database dump complete
---
+CREATE TABLE `versions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `rubygem_id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `prerelease` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_versions_on_rubygem_id_and_number` (`rubygem_id`,`number`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO schema_migrations (version) VALUES ('20101111043525');
 
@@ -223,3 +66,5 @@ INSERT INTO schema_migrations (version) VALUES ('20110103015306');
 INSERT INTO schema_migrations (version) VALUES ('20110103015542');
 
 INSERT INTO schema_migrations (version) VALUES ('20110108030608');
+
+INSERT INTO schema_migrations (version) VALUES ('20110313153714');
