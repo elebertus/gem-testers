d55f5e7fc823ac47264a501dd31b06e07786bcf7
Erik Hollensbe <erik@hollensbe.org>
2011-05-08 01:21:45 -0400

    Add author names in more spots

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,7 @@
 class AuthorsController < ApplicationController
   def gems
-    @gems = sort_gems(Author.where(:id => params[:id]).first.rubygems)
+    @author = Author.where(:id => params[:id]).first
+    @gems = sort_gems(@author.rubygems)
     render 'browse/gems'
   end
 end
